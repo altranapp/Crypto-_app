5e5c19e1fe422604a719d281b32c9cf121dac983
altranapp <brandygoodluck11@gmail.com>
2026-04-22 14:35:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,4 +14,5 @@ app.use('/api/user', require('./routes/user'));
 app.use('/api/admin', require('./routes/admin'));
 app.use('/api/kyc', require('./routes/kyc'));
 
-app.listen(5000, () => console.log("Server running on 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running on " + PORT));
