6384974ae0d9bafa38921fd61e90397bfc6595ec
altranapp <brandygoodluck11@gmail.com>
2026-04-23 14:35:56 +0100

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const transactionSchema = new mongoose.Schema({
   userId: String,
-  type: String, // deposit / withdraw
+  type: String,
   amount: Number,
   status: { type: String, default: "pending" }
-});
+}, { timestamps: true }); // ✅ IMPORTANT
 
 export default mongoose.model("Transaction", transactionSchema);
