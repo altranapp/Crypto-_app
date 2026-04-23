0cd7ac02d8cc153b426c56073fd26696d5b26b50
altranapp <brandygoodluck11@gmail.com>
2026-04-23 18:59:47 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  email: String,
+  password: String,
   balance: { type: Number, default: 0 },
   tier: { type: Number, default: null },
   role: { type: String, default: "user" }
-}, { timestamps: true });
+});
 
-export default mongoose.model("User", userSchema);https://crypto-app-y5vn.onrender.com
+export default mongoose.model("User", userSchema);
